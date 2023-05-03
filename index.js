f0df0d389015a88516854c9249cd0b958c674007
Shrabon Ahamed <shrabonahamed106@gmail.com>
2023-05-04 04:48:42 +0600

    added service post data from databease

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express();
 const port = 5000;
@@ -9,8 +9,13 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://travelAirlines:ZOpB5Z3u3EH9ZqCi@cluster0.qm6ghoc.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri)
+const uri = "mongodb://travelAirlines:ZOpB5Z3u3EH9ZqCi@ac-gpqk4f0-shard-00-00.qm6ghoc.mongodb.net:27017,ac-gpqk4f0-shard-00-01.qm6ghoc.mongodb.net:27017,ac-gpqk4f0-shard-00-02.qm6ghoc.mongodb.net:27017/?ssl=true&replicaSet=atlas-rcscp7-shard-0&authSource=admin&retryWrites=true&w=majority";
+MongoClient.connect(uri, function (err, client) {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -25,20 +30,39 @@ async function run() {
     try {
         const serviceCollcetions = client.db('travelAirlines').collection('travelServices');
         const teamsCollcetions = client.db('travelAirlines').collection('teamsInfo');
+        const reviewsCollections = client.db('travelAirlines').collection('serviceReviews');
+
+
         // get service data from database.
-        app.get('/services', async(req, res)=>{
+        app.get('/services', async (req, res) => {
             const query = {};
             const result = await serviceCollcetions.find(query).toArray();
             res.send(result);
         })
 
-        // geting teams data from database.
+        // get specific service data from database.
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollcetions.findOne(query);
+            res.send(result);
+        })
 
-        app.get('/teams', async(req, res)=>{
+        // geting teams data from database.
+        app.get('/teams', async (req, res) => {
             const query = {};
             const result = await teamsCollcetions.find(query).toArray();
             res.send(result);
         })
+
+
+        // post reviews from database. 
+        app.post('/reviews', async(req, res)=>{
+            const reivew = req.body;
+            const result = await reviewsCollections.insertOne(reivew);
+            res.send(result);
+        })
+        
 
 
 
